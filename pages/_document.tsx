d0f1734b950b4c8af8a657887128b48599954d69
Denis Homolik <alfonz@homolik.cz>
2020-12-10 00:14:42 +0100

    Optimalize resources

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,7 +16,29 @@ class MyDocument extends Document {
           />
 
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Castoro&display=swap" rel="stylesheet" />
+          <style
+            dangerouslySetInnerHTML={{
+              __html: `
+              /* latin-ext */
+              @font-face {
+                font-family: 'Castoro';
+                font-style: normal;
+                font-weight: 400;
+                font-display: swap;
+                src: url(https://fonts.gstatic.com/s/castoro/v1/1q2GY5yMCld3-O4cLYFOzcQdjETZfw.woff2) format('woff2');
+                unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+              }
+              /* latin */
+              @font-face {
+                font-family: 'Castoro';
+                font-style: normal;
+                font-weight: 400;
+                font-display: swap;
+                src: url(https://fonts.gstatic.com/s/castoro/v1/1q2GY5yMCld3-O4cLY9OzcQdjEQ.woff2) format('woff2');
+                unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+              }`
+            }}
+          />
 
           {/* <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png" />
